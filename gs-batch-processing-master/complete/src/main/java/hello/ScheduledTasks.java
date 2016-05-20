074ee69f94633f15a7f237e023b616f3fdf5fe6d
AshokChaitanya <ashokchaitanya.s@gmail.com>
2016-05-20 03:46:41 -0700

    Add files via upload

---
+++ gs-batch-processing-master/complete/src/main/java/hello/ScheduledTasks.java
@@ -0,0 +1,49 @@
+package hello;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+    
+@Autowired
+private JobLauncher launcher;
+
+@Autowired
+private Job job;
+
+private JobExecution execution;
+
+
+private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+   // @Scheduled(fixedRate = 5000)
+     @Scheduled(cron="*/5 * * * * *")
+    public void reportCurrentTime() {
+        try {
+
+            execution = launcher.run(job, new JobParameters());
+
+            System.out.println("Execution status: "+ execution.getStatus());
+
+        } catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException | JobParametersInvalidException e) {        
+
+            e.printStackTrace();
+
+        }
+
+        System.out.println("The time is now " + dateFormat.format(new Date()));
+    }
+}
